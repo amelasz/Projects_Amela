fd2481bf511c5fe13833d98db339b5bde83ce274
amelasz <amela.samardzic@student.htldornbirn.at>
2022-10-17 15:04:28 +0200

    OO - 02 Das erste Objekt fertig

---
+++ OO_Cars/src/Cars/Car.java
@@ -0,0 +1,11 @@
+package Cars;
+
+public class Car {
+    // Instanz / Gedächtnisvariablen
+
+    // DONT DO THAT LATER
+    public int fuelConsumption;
+    public String brand;
+    public String serialNumber;
+    private String color;
+}
---
+++ OO_Cars/src/Cars/Main.java
@@ -0,0 +1,40 @@
+package Cars;
+
+public class Main {
+    public static void main(String[] args) {
+
+    Car c1 = new Car();
+    c1.brand = "Benz";
+    c1.fuelConsumption = 7;
+    c1.serialNumber = "B0001";
+
+    Car c2 = new Car();
+    c2.brand = "Audi";
+    c2.fuelConsumption = 5;
+    c2.serialNumber = "A0001";
+
+    System.out.println(c2.serialNumber);
+    System.out.println(c1.brand);
+
+/*
+        // DONT DONT DONT
+
+        int car1FuelConsumption = 7;
+        String car1Brand = "Benz";
+        String car1SerialNumber = "B0001";
+
+        int car2FuelConsumption = 5;
+        String car2Brand = "Audi";
+        String car2SerialNumber = "A0001";
+
+
+
+        String[] cars = {"7;Benz;B0001", "5;Audi;A0001"};
+
+        String[] car1 = cars[0].split(regex: ";");
+
+        // DONT DONT DONT
+
+ */
+    }
+}
